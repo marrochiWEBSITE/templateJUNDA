57ea5f093adb2c2d66ea647fd08fc66c4ecf9573
marrochiWEBSITE <marrochiWEBSITE@gmail.com>
2019-12-25 18:31:17 +0700

    update 2

--- js/cafe-jq.js
+++ js/cafe-jq.js
@@ -3,5 +3,14 @@
 
 $(document).ready(function(){
    $('.show-1').fadeIn(3500);
-
 });
+
+// ----------------------------- home
+window.setTimeout("waktu()",1000);
+    function waktu() {
+        var tanggal = new Date();
+        setTimeout("waktu()",1000);
+        document.getElementById("jam").innerHTML = tanggal.getHours();
+        document.getElementById("menit").innerHTML = tanggal.getMinutes();
+        document.getElementById("detik").innerHTML = tanggal.getSeconds();
+    }
